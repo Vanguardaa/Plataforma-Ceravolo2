f821cbc0d8511398e04d926ac0c140e8efb94140
Matheus M. Prado <142365200+Vanguardaa@users.noreply.github.com>
2023-09-12 21:05:22 -0300

    teste com responsividade e criando termos de uso

--- login/script.js
+++ login/script.js
@@ -14,6 +14,6 @@ document.querySelector('#btnSignin').addEventListener('click', () => {
 document.querySelector('#btnSignup').addEventListener('click', () => {
     formSignin.style.left = "-450px"
     formSignup.style.left = "25px"
-    btnColor.style.left = "94.8px"
+    btnColor.style.left = "96.8px"
     btnColor.style.width = "128px";
 });
